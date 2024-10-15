cb8eee01f9d333ddf78d835930f4c4495a4c7b4d
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2024-10-15 10:30:29 -0300

    1ª Commit

---
+++ Aluno.java
@@ -0,0 +1,9 @@
+/**
+ * Classe para o cadastro de alunos genéricos de uma faculdade
+ * @author Erick Gomes Barbosa
+ */
+
+public class Aluno{
+    String nome, sexo, endereço, telefone, email;
+    Integer idade, matrícula;
+}
